bffb324ffd59e87cfb16e20ea59b9fb6209ea26b
Tom Mrazauskas <tom@mrazauskas.de>
2023-11-16 13:32:37 +0200

    fix overflow

--- components/Advice.tsx
+++ components/Advice.tsx
@@ -3,7 +3,7 @@ import type { ReactElement, ComponentProps } from "react";
 
 export function Note({ children }: ComponentProps<"div">): ReactElement {
   return (
-    <div className="mt-6 flex rounded-md border border-gray-400 py-2 pr-4 dark:border-gray-500">
+    <div className="mt-6 flex overflow-x-auto rounded-md border border-gray-400 py-2 pr-4 dark:border-gray-500">
       <div className="select-none pl-3 pr-2 text-gray-400 dark:text-gray-500">
         <NoteIcon className="inline" size={20} />
       </div>
@@ -14,7 +14,7 @@ export function Note({ children }: ComponentProps<"div">): ReactElement {
 
 export function Tip({ children }: ComponentProps<"div">): ReactElement {
   return (
-    <div className="mt-6 flex rounded-md border border-blue-ribbon-700 py-2 pr-4 dark:border-blue-ribbon-500">
+    <div className="mt-6 flex overflow-x-auto rounded-md border border-blue-ribbon-700 py-2 pr-4 dark:border-blue-ribbon-500">
       <div className="select-none pl-3 pr-2 text-blue-ribbon-700 dark:text-blue-ribbon-500">
         <TipIcon className="inline" size={20} />
       </div>
@@ -25,7 +25,7 @@ export function Tip({ children }: ComponentProps<"div">): ReactElement {
 
 export function Warning({ children }: ComponentProps<"div">): ReactElement {
   return (
-    <div className="mt-6 flex rounded-md border border-blaze-orange-700 py-2 pr-4 dark:border-blaze-orange-500">
+    <div className="mt-6 flex overflow-x-auto rounded-md border border-blaze-orange-700 py-2 pr-4 dark:border-blaze-orange-500">
       <div className="select-none pl-3 pr-2 text-blaze-orange-700 dark:text-blaze-orange-500">
         <WarningIcon className="inline" size={20} />
       </div>
