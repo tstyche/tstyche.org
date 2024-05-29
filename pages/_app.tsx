ce69ab09b2ca504fc0b792f2a1937a4726025c7b
Tom Mrazauskas <tom@mrazauskas.de>
2024-05-29 13:08:56 +0300

    use `fontsource`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
-import { ibmPlexMono, ibmPlexSans } from "@/styles/fonts";
+import '@fontsource/ibm-plex-mono/400.css';
+import '@fontsource/ibm-plex-mono/600.css';
+import '@fontsource/ibm-plex-sans/400.css';
+import '@fontsource/ibm-plex-sans/600.css';
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <main className={`${ibmPlexSans.variable} ${ibmPlexMono.variable}`}>
-      <Component {...pageProps} />
-    </main>
-  );
+  return <Component {...pageProps} />;
 }
