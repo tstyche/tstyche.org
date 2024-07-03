56f01439bbf513fe40d5febb7207efd2ba312603
Tom Mrazauskas <tom@mrazauskas.de>
2024-07-03 14:16:57 +0300

    rework Introduction and Get Started pages

--- components/Essentials.tsx
+++ components/Essentials.tsx
@@ -25,9 +25,9 @@ export function Essentials() {
         <p className="mt-10 sm:text-lg lg:text-xl">
           <Link
             className="mr-4 rounded-none border-b border-blue-ribbon-700 py-2 text-blue-ribbon-700 dark:border-blue-ribbon-500 dark:text-blue-ribbon-500 sm:mr-6"
-            href="/guide/introduction"
+            href="/guide/get-started"
           >
-            Introduction
+            Get started
             <ArrowRight className="inline pl-1 align-bottom" size={24} />
           </Link>
           <Link
