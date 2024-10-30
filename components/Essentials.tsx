086c9f0fd0263b7137e853b0cc99a4c5ae38c580
Tom Mrazauskas <tom@mrazauskas.de>
2024-10-30 13:30:33 +0200

    anounce `rc`

--- components/Essentials.tsx
+++ components/Essentials.tsx
@@ -5,9 +5,8 @@ import { Code } from "./Code";
 export function Essentials() {
   return (
     <>
-      <div className="rounded-md text-center border text-blaze-orange-700 border-blaze-orange-700 py-2 dark:text-blaze-orange-500 dark:border-blaze-orange-500">
-        <Link href="/releases/tstyche-3">
-          Try out TSTyche 3.0 (beta)
+      <div className="rounded-md text-center border text-blue-ribbon-700 border-blue-ribbon-700 py-2 dark:text-blue-ribbon-500 dark:border-blue-ribbon-500">        <Link href="/releases/tstyche-3">
+          TSTyche 3.0 is now available
           <ArrowRight className="inline pl-1 align-bottom" size={22} />
         </Link>
       </div>
