665849dcb23e4ce8662e39ecfdb0ebeec0997d1d
mrazauskas <tom@mrazauskas.de>
2025-09-14 07:51:46 +0300

    move type utilities

--- content/reference/_meta.js
+++ content/reference/_meta.js
@@ -1,6 +1,7 @@
 export default {
   "expect-api": "Expect API",
   "testing-api": "Testing API",
+  "type-utilities": "Type Utilities",
   directives: "Comment Directives",
   "config-file": "Configuration File",
   "command-line": "Command Line",
