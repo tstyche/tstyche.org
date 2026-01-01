28d47f0feb6409b6ff233e1da0bbcd32a2732b61
mrazauskas <tom@mrazauskas.de>
2026-01-01 11:43:42 +0200

    update examples

--- content/guides/_meta.js
+++ content/guides/_meta.js
@@ -2,6 +2,5 @@ export default {
   "typescript-versions": "TypeScript Versions",
   "template-test-files": "Template Test Files",
   reporters: "Reporters",
-  plugins: "Plugins",
   "programmatic-usage": "Programmatic Usage",
 };
