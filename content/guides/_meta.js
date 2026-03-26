c2be9f4fde1358e7d377bf5eb1fdeddaf80f49bd
mrazauskas <tom@mrazauskas.de>
2026-03-26 11:19:45 +0200

    add Expect Errors page

--- content/guides/_meta.js
+++ content/guides/_meta.js
@@ -1,5 +1,6 @@
 export default {
   "typescript-versions": "TypeScript Versions",
+  "expect-errors": "Expect Errors",
   "template-test-files": "Template Test Files",
   reporters: "Reporters",
   "programmatic-usage": "Programmatic Usage",
